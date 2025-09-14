21eee1ccbe00a925dc2328d7b4890150ff234a05
shzdasim <shzd.asim@gmail.com>
2025-09-14 12:42:05 +0500

    UPDATED STOCK

--- database/migrations/2025_08_10_151532_create_products_table.php
+++ database/migrations/2025_08_10_151532_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('product_code')->unique();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->string('image')->nullable();
             $table->string('formulation')->nullable();
             $table->text('description')->nullable();
